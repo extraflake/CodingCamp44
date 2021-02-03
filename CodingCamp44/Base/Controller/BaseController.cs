1ae7afbdcb32c5e15e6998d24e17b8c1092fd8a0
Tresnha Wira Hisbullah <tresnamaku97@gmail.com>
2021-02-03 10:03:23 +0700

    First Update

--- CodingCamp44/Base/Controller/BaseController.cs
+++ CodingCamp44/Base/Controller/BaseController.cs
@@ -16,9 +16,9 @@ namespace CodingCamp44.Base.Controller
     [Route("api/[controller]")]
     [ApiController]
 
-    public class BaseController<Entity, Repository> : ControllerBase
+    public class BaseController<Entity, Repository, Id> : ControllerBase
         where Entity : class
-        where Repository : IRepository<Entity>
+        where Repository : IRepository<Entity, Id>
     {
         private readonly Repository repository;
 
@@ -26,19 +26,19 @@ namespace CodingCamp44.Base.Controller
         {
             this.repository = repository;
         }
-        
+
         [HttpGet]
         public ActionResult Get()
         {
             var data = repository.Get();
-            return (data != null) ? (ActionResult)Ok(new { data = data, status = "Ok" }) : StatusCode(500, new { data = data, status = "Internal Server Error", errorMessage = "Cannot get the data" });        
+            return (data != null) ? (ActionResult)Ok(new { data = data, status = "Ok" }) : StatusCode(500, new { data = data, status = "Internal Server Error", errorMessage = "Cannot get the data" });
         }
 
         [HttpGet("{id}")]
-        public ActionResult Get(int id)
+        public ActionResult Get(Id id)
         {
             var data = repository.Get(id);
-            return (data != null) ? (ActionResult)Ok(new { data = data, status = "Ok" }) : NotFound(new { data = data, status = "Not Found", errorMessage = "ID is not identified" }); 
+            return (data != null) ? (ActionResult)Ok(new { data = data, status = "Ok" }) : NotFound(new { data = data, status = "Not Found", errorMessage = "ID is not identified" });
         }
 
         [HttpPost]
@@ -47,15 +47,15 @@ namespace CodingCamp44.Base.Controller
             if (entity == null)
                 return BadRequest(new { status = "Bad Request", errorMessage = "All input data need to be inserted" });
             var data = repository.Create(entity);
-            return (data != 0) ? (ActionResult)Ok(new { data = data, status = "Ok" }) : StatusCode(500, new { status = "Internal Server Error", errorMessage = "Failed to input the data" }); 
+            return (data != 0) ? (ActionResult)Ok(new { data = data, status = "Ok" }) : StatusCode(500, new { status = "Internal Server Error", errorMessage = "Failed to input the data" });
         }
 
         [HttpPut]
-        public ActionResult Update(int id,Entity entity)
+        public ActionResult Update(Id id, Entity entity)
         {
             if (repository.Get(id) != null)
             {
-            	return NotFound("ID Not Found");
+                return NotFound("ID Not Found");
             }
             else
             {
@@ -66,23 +66,23 @@ namespace CodingCamp44.Base.Controller
                 }
                 catch (Exception)
                 {
-		            return StatusCode(500, new { status = "Internal Server Error", errorMessage = "Failed to input the data" });
+                    return StatusCode(500, new { status = "Internal Server Error", errorMessage = "Failed to input the data" });
                 }
             }
         }
 
         [HttpDelete("{id}")]
-        public ActionResult Delete(int id)
+        public ActionResult Delete(Id id)
         {
-            if (repository.Get(id) == null) 
+            if (repository.Get(id) == null)
             {
                 return NotFound("ID Not Found");
             }
             else
             {
                 var data = repository.Delete(id);
-                return (data != 0) ? (ActionResult)Ok(new {status = "Ok"}) : StatusCode(500, new {status = "Internal Server Error" });
+                return (data != 0) ? (ActionResult)Ok(new { status = "Ok" }) : StatusCode(500, new { status = "Internal Server Error" });
             }
-        }    
+        }
     }
 }
--- CodingCamp44/Context/MyContext.cs
+++ CodingCamp44/Context/MyContext.cs
@@ -13,9 +13,9 @@ namespace CodingCamp44.Context
         public MyContext(DbContextOptions<MyContext> options) : base(options) { }
 
         public DbSet<Role> Roles { get; set; }
-	    public DbSet<Education> Education { get; set; }
-	    public DbSet<Job> Jobs { get; set; }
-	    public DbSet<University> University { get; set; }
+        public DbSet<Education> Education { get; set; }
+        public DbSet<Job> Jobs { get; set; }
+        public DbSet<University> University { get; set; }
         public DbSet<Profiling> Profiling { get; set; }
         public DbSet<Person> Persons { get; set; }
         public DbSet<Account> Accounts { get; set; }
@@ -23,10 +23,10 @@ namespace CodingCamp44.Context
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Account>().HasOne(a => a.Person).WithOne(b => b.Account).HasForeignKey<Account>(a => a.NIK);
-	        modelBuilder.Entity<Education>().HasOne(x => x.University);
+            modelBuilder.Entity<Education>().HasOne(x => x.University);
             modelBuilder.Entity<Profiling>().HasOne(x => x.Education);
             modelBuilder.Entity<Profiling>().HasOne(a => a.Person).WithOne(b => b.Profiling).HasForeignKey<Profiling>(a => a.NIK);
-	        modelBuilder.Entity<Profiling>().HasOne(x => x.Job).WithMany(z => z.Profilings);
+            modelBuilder.Entity<Profiling>().HasOne(x => x.Job).WithMany(z => z.Profilings);
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
