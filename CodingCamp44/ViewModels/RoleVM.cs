e3b4323ccfbdfb83155bd7dcb721654a488e2c70
Tresnha Wira Hisbullah <tresnamaku97@gmail.com>
2021-02-03 21:23:19 +0700

    ADD MODUL RoleVM
    Create New View Model Class in ViewModels Folder

---
+++ CodingCamp44/ViewModels/RoleVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingCamp44.ViewModels
+{
+    public class RoleVM
+    {
+        public int id { get; set; }
+        public int name { get; set; }
+    }
+}
