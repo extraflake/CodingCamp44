6c08cbda9b1504c1ed0a24c656a153c151741738
Tresnha Wira Hisbullah <tresnamaku97@gmail.com>
2021-02-04 13:44:32 +0700

    Update default folder on my project

--- WebMVC/Controllers/RoleController.cs
+++ WebMVC/Controllers/RoleController.cs
@@ -10,46 +10,47 @@ using CodingCamp44.Models;
 
 namespace WebMVC.Controllers
 {
+    [Route("[controller]")]
     public class RoleController : Controller
     {
-        public async Task<ActionResult> Index()
+        //public async Task<ActionResult> Index()
         
-        {
-            List<RoleVM> roleList = new List<RoleVM>();
-            using (var httpClient = new HttpClient())
-            {
-                using (var response = await httpClient.GetAsync("https://localhost:44383/api/Role"))
-                {
-                    string apiResponse = await response.Content.ReadAsStringAsync();
-                    roleList = JsonConvert.DeserializeObject<List<RoleVM>>(apiResponse);
-                }
-            }
-            return Json(roleList);
-        }
+        //{
+        //    List<RoleVM> roleList = new List<RoleVM>();
+        //    using (var httpClient = new HttpClient())
+        //    {
+        //        using (var response = await httpClient.GetAsync("https://localhost:44383/api/Role"))
+        //        {
+        //            string apiResponse = await response.Content.ReadAsStringAsync();
+        //            roleList = JsonConvert.DeserializeObject<List<RoleVM>>(apiResponse);
+        //        }
+        //    }
+        //    return Json(roleList);
+        //}
 
-        public async Task<IActionResult> UpdateRole(int id)
-        {
-            RoleVM roleVM = new RoleVM();
-            using (var httpClient = new HttpClient())
-            {
-                using (var response = await httpClient.GetAsync("https://localhost:44383/api/Role" + id))
-                {
-                    string apiResponse = await response.Content.ReadAsStringAsync();
-                    roleVM = JsonConvert.DeserializeObject<RoleVM>(apiResponse);
-                }
-            }
-            return View(roleVM);
-        }
+        //public async Task<IActionResult> UpdateRole(int id)
+        //{
+        //    RoleVM roleVM = new RoleVM();
+        //    using (var httpClient = new HttpClient())
+        //    {
+        //        using (var response = await httpClient.GetAsync("https://localhost:44383/api/Role" + id))
+        //        {
+        //            string apiResponse = await response.Content.ReadAsStringAsync();
+        //            roleVM = JsonConvert.DeserializeObject<RoleVM>(apiResponse);
+        //        }
+        //    }
+        //    return View(roleVM);
+        //}
 
-        [HttpPost("[update]")]
-        public async Task<IActionResult> UpdateRole([FromBody] RoleVM reservation)
+        [HttpPost("update")]
+        public async Task<IActionResult> UpdateRole([FromBody] RoleVM roleVM)
         {
             RoleVM receivedRole = new RoleVM();
             using (var httpClient = new HttpClient())
             {
                 var content = new MultipartFormDataContent();
-                content.Add(new StringContent(reservation.Id.ToString()), "Id");
-                content.Add(new StringContent(reservation.Name), "Name");
+                content.Add(new StringContent(roleVM.Id.ToString()), "Id");
+                content.Add(new StringContent(roleVM.Name), "Name");
 
                 using (var response = await httpClient.PutAsync("https://localhost:44383/api/Role", content))
                 {
@@ -58,7 +59,7 @@ namespace WebMVC.Controllers
                     receivedRole = JsonConvert.DeserializeObject<RoleVM>(apiResponse);
                 }
             }
-            return View(receivedRole);
+            return Json(receivedRole);
         }
     }
 }
