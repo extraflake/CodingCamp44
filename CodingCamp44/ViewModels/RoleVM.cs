a2e2323dfcc78bf92e8eb76455cb40c2b4d67d8d
Gany Gemilang <64389068+GanyGemilang@users.noreply.github.com>
2021-02-05 08:41:29 +0700

    ADD MODUL ROLEVM
    CRUD RoleController WebMVC

---
+++ CodingCamp44/ViewModels/RoleVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingCamp44.ViewModels
+{
+    public class RoleVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    /*public class RoleVM
+    {
+        public List<DataRoleVM> data { get; set; }
+    }
+
+    public class DataRoleVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }*/
+}
---
+++ WebMVC/Controllers/RoleController.cs
@@ -0,0 +1,96 @@
+﻿using CodingCamp44.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebMVC.Controllers
+{
+    [Route("[controller]")]
+    public class RoleController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            RoleVM roleList = new RoleVM();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:44383/api/Role"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    roleList = JsonConvert.DeserializeObject<RoleVM>(apiResponse);
+                    return Json(roleList);
+                }
+            }
+            //return View(roleList);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] RoleVM roleVM)
+        {
+            RoleVM addRole = new RoleVM();
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(roleVM), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PostAsync("https://localhost:44383/api/Role/", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    addRole = JsonConvert.DeserializeObject<RoleVM>(apiResponse);
+                }
+            }
+            return Ok(new { data = addRole });
+        }
+
+        //[HttpPut]
+        /*public async Task<IActionResult> Update(int id)
+        {
+            RoleVM roleList = new RoleVM();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:44383/api/Role" + id))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    roleList = JsonConvert.DeserializeObject<RoleVM>(apiResponse);
+                    return Json(roleList);
+                }
+            }
+            //return View(roleList);
+        }*/
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] RoleVM roleVM)
+        {
+            RoleVM roleList = new RoleVM();
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(roleVM), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PutAsync("https://localhost:44383/api/Role/", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    ViewBag.Result = "Success";
+                    roleList = JsonConvert.DeserializeObject<RoleVM>(apiResponse);
+                }
+            }
+            return Ok(new { data = roleList });
+            //return View(roleList);
+        }
+        
+        [HttpDelete("{roleId}")]
+        public async Task<IActionResult> Delete(int roleId)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.DeleteAsync("https://localhost:44383/api/Role/" + roleId))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    return Ok(new { data = apiResponse });
+                }
+            }
+        }
+    }
+}
